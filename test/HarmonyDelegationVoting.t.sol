ac0327410dc27d7d185f09b64dc8fd560e29c39e
mzfshark <mzfshark@gmail.com>
2026-01-16 04:23:13 -0300

    chore: apply repository updates

--- test/HarmonyDelegationVoting.t.sol
+++ test/HarmonyDelegationVoting.t.sol
@@ -6,6 +6,7 @@ import {TestBase} from "./lib/TestBase.sol";
 import {DAO} from "@aragon/osx/core/dao/DAO.sol";
 import {ProxyLib} from "@aragon/osx-commons-contracts/src/utils/deployment/ProxyLib.sol";
 import {HarmonyDelegationVotingPlugin} from "../src/harmony/HarmonyDelegationVotingPlugin.sol";
+import {HarmonyVotingBase} from "../src/harmony/HarmonyVotingBase.sol";
 
 contract HarmonyDelegationVotingTest is TestBase {
     DAO dao;
@@ -70,5 +71,84 @@ contract HarmonyDelegationVotingTest is TestBase {
         vm.prank(carol);
         vm.expectRevert();
         plugin.setValidatorAddress(newValidator);
+    }
+
+    function _hashPair(bytes32 a, bytes32 b) internal pure returns (bytes32) {
+        return a < b ? keccak256(abi.encodePacked(a, b)) : keccak256(abi.encodePacked(b, a));
+    }
+
+    function test_Flow_CastThenSnapshotThenSubmitPowerThenClose() external {
+        uint64 startDate = uint64(block.timestamp);
+        uint64 endDate = uint64(block.timestamp + 10_000);
+        uint64 snapshotBlock = uint64(block.number + 10);
+
+        vm.deal(carol, 1 ether);
+        vm.deal(david, 1 ether);
+
+        vm.prank(carol);
+        uint256 proposalId = plugin.createProposal(bytes32("delegation"), startDate, endDate, snapshotBlock);
+
+        vm.prank(carol);
+        plugin.castVote(proposalId, HarmonyVotingBase.VoteOption.Yes);
+
+        vm.prank(david);
+        plugin.castVote(proposalId, HarmonyVotingBase.VoteOption.No);
+
+        vm.prank(oracle);
+        vm.expectRevert("SNAPSHOT_NOT_REACHED");
+        plugin.setMerkleRoot(proposalId, bytes32(uint256(1)), 1);
+
+        vm.roll(uint256(snapshotBlock));
+
+        uint256 carolPower = 100;
+        uint256 davidPower = 50;
+
+        bytes32 carolLeaf = keccak256(abi.encodePacked(carol, carolPower));
+        bytes32 davidLeaf = keccak256(abi.encodePacked(david, davidPower));
+        bytes32 root = _hashPair(carolLeaf, davidLeaf);
+
+        vm.prank(oracle);
+        vm.expectRevert("FINALIZATION_NOT_STARTED");
+        plugin.setMerkleRoot(proposalId, root, carolPower + davidPower);
+
+        vm.warp(uint256(endDate));
+
+        vm.prank(oracle);
+        plugin.setMerkleRoot(proposalId, root, carolPower + davidPower);
+
+        bytes32[] memory proofCarol = new bytes32[](1);
+        proofCarol[0] = davidLeaf;
+
+        vm.prank(alice);
+        plugin.submitVotingPower(proposalId, carol, carolPower, proofCarol);
+
+        bytes32[] memory proofDavid = new bytes32[](1);
+        proofDavid[0] = carolLeaf;
+
+        vm.prank(alice);
+        plugin.submitVotingPower(proposalId, david, davidPower, proofDavid);
+
+        vm.prank(carol);
+        vm.expectRevert("VOTING_ENDED");
+        plugin.castVote(proposalId, HarmonyVotingBase.VoteOption.No);
+
+        HarmonyDelegationVotingPlugin.ProposalData memory p = plugin.getProposal(proposalId);
+        assertEq(p.yes, carolPower);
+        assertEq(p.no, davidPower);
+        assertEq(p.abstain, 0);
+
+        vm.expectRevert("FINALIZATION_NOT_ENDED");
+        plugin.closeProposal(proposalId);
+
+        vm.prank(oracle);
+        plugin.oracleCloseProposal(proposalId);
+
+        p = plugin.getProposal(proposalId);
+        assertTrue(p.closed);
+        assertTrue(p.passed);
+
+        vm.prank(david);
+        vm.expectRevert("PROPOSAL_CLOSED");
+        plugin.castVote(proposalId, HarmonyVotingBase.VoteOption.Yes);
     }
 }
